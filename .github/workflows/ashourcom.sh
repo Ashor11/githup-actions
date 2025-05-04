35bd35e715bcecfb26440346f4d9115b1fa8833c
Ashour11 <ashouryasser11@gmail.com>
2025-05-04 15:44:28 +0300

    script ashour

---
+++ .github/workflows/ashourcom.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Running ashourcom.sh"
+echo "This is a placeholder script for the ashourcom workflow."
+echo "You can add your custom commands here."
+echo "This script is executed as part of the GitHub Actions workflow."
+echo "You can use this script to automate tasks, run tests, or perform any other actions you need."
